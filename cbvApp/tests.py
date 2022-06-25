c9ba7266593a7fc20dbbce7e24e79e0bdc42a7fe
SireenAtGithub <er.sireen@gmail.com>
2022-06-25 14:41:51 +0530

    New Repo Structure

--- cbvApp/tests.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-
-# Create your tests here.
-
-def hello_guyys():
-    print('tesset')
-    print("commit on second branch")
-    print("commit on second branch again")
